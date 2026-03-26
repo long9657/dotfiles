3d06fa51f02eb6c57080ceb2fccbbf7c3bb98a73
long9657 <longlehien9657@gmail.com>
2026-03-26 15:32:20 +0700

    add: kanshi package

--- others/setup.sh
+++ others/setup.sh
@@ -3,7 +3,7 @@ set -e
 
 PKGS=(
     'stow' 'bat' 'nvidia-inst' 'tmux' 'lsd' 'zoxide' 'fzf' 'ripgrep' 'fd' 'tree'
-    'discord' 'qutebrowser' 'neovim' 'python-pip' 'nodejs' 'npm' 'kitty'
+    'discord' 'qutebrowser' 'neovim' 'python-pip' 'nodejs' 'npm' 'kitty' 'kanshi'
     'jdk-openjdk' 'ttf-inconsolata-nerd' 'ttf-jetbrains-mono-nerd' 'zsh' 'tree-sitter-cli'
 )
 AUR=('ghostty' 'microsoft-edge-stable-bin' 'postman-bin')
