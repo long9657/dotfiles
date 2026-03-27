5b2f0747f8ee07d7d39e13160bab01429609fd7b
long9657 <longlehien9657@gmail.com>
2026-03-28 01:16:57 +0700

    chore: monitor

--- monitor/.screenlayout/monitor.sh
+++ monitor/.screenlayout/monitor.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-4 --off
+
+if xrandr | grep "HDMI-0 connected"; then
+    # CÓ màn rời: Dùng màn rời làm primary, TẮT màn laptop
+    xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off \
+           --output HDMI-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal \
+           --output DP-4 --off
+else
+    # KHÔNG có màn rời: Bật màn laptop (DP-4), TẮT màn HDMI
+    xrandr --output HDMI-0 --off \
+           --output DP-4 --primary --mode 1920x1080 --rate 165.01 --pos 0x0 --rotate normal
+fi
