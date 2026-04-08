3569f56189efabd0856b52ad9366efbb31793920
long9657 <90248261+long9657@users.noreply.github.com>
2026-04-08 23:42:59 +0700

    Add bluez and bluez-utils to package list

--- others/setup.sh
+++ others/setup.sh
@@ -3,7 +3,7 @@ set -e
 
 PKGS=(
     'stow' 'bat' 'tmux' 'lsd' 'zoxide' 'fzf' 'ripgrep' 'fd' 'tree'
-    'discord' 'qutebrowser' 'bob' 'python-pip' 'nodejs' 'npm' 'picom' 'xclip'
+    'discord' 'qutebrowser' 'bob' 'python-pip' 'nodejs' 'npm' 'picom' 'xclip' 'bluez' 'bluez-utils'
     'jdk-openjdk' 'ttf-inconsolata-nerd' 'ttf-jetbrains-mono-nerd' 'zsh' 'tree-sitter-cli' 
 #'nvidia-inst' 'kanshi' 'xwayland-satellite' 'kitty' 'kanshi'
 )
